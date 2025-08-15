02e2f0b4f60c25b02eab679a355397d00e9c8abd
Pateriya27 <hppateriya9165@gmail.com>
2025-08-16 02:12:28 +0530

    first upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+    for (let code in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerHTML = code;
+        newOption.value = code;
+        select.append(newOption);
+
+        if (select.name === "from" && code === "USD") {
+            newOption.selected = "selected";
+        } else if (select.name === "to" && code === "INR") {
+            newOption.selected = "selected";
+        }
+    }
+
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    })
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector('img');
+    img.src = newSrc;
+
+    // if (select.name === "from") {
+    //     document.getElementsByTagName("img")[0].src = newSrc;
+    // } else {
+    //     document.getElementsByTagName("img")[1].src = newSrc;
+    // }
+
+}
+
+
+
+const updateExchange = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    let finalAmt = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
+}
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchange();
+});
+
+window.addEventListener("load", () => {
+    updateExchange();
+})
